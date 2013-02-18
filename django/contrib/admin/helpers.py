472c871363bad5a03e49492bafd4f65357b286a8
Stefan Klug <klug.stefan@gmx.de>
2013-02-18 13:59:47 +0100

    remove fk and id fields from new nested_inlines, because these are not known

--- django/contrib/admin/helpers.py
+++ django/contrib/admin/helpers.py
@@ -328,9 +328,23 @@ class AdminErrorList(forms.util.ErrorList):
         if form.is_bound:
             self.extend(list(six.itervalues(form.errors)))
             for inline_formset in inline_formsets:
-                self.extend(inline_formset.non_form_errors())
-                for errors_in_inline_form in inline_formset.errors:
-                    self.extend(list(six.itervalues(errors_in_inline_form)))
+                self._add_formset_recursive(inline_formset)
+                    
+    def _add_formset_recursive(self, formset):
+        #check if it is a wrapped formset
+        if isinstance(formset, InlineAdminFormSet):
+            formset = formset.formset
+            
+        self.extend(formset.non_form_errors())
+        for errors_in_inline_form in formset.errors:
+            self.extend(list(six.itervalues(errors_in_inline_form)))
+        
+        #support for nested formsets            
+        for form in formset:
+            if hasattr(form, 'nested_formsets'):
+                for fs in form.nested_formsets:
+                    self._add_formset_recursive(fs)
+                    
 
 def normalize_fieldsets(fieldsets):
     """
